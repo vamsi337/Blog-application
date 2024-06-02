1fc9fa9a09fc484bbd1e7d821db149f3ce5027e0
Vamsi Krishna <vamsijvk0337@gmail.com>
2024-06-02 19:33:00 +0530

    feat:002: Created route folder

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@
 const express = require('express');
 const dotenv= require('dotenv')
 dotenv.config();
-
+const expressLayout= require('express-ejs-layouts')
 const app = express();
+app.use(expressLayout);
 
+app.use(express.static('public'));
+app.use('/app', require('./routes/homepage.route'))
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 const PORT= 5000 || process.env.PORT
 
-app.get('/home', (req,res)=>{
-    res.send('Hi')
-})
+
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`)
 })
---
+++ src/routes/homepage.route.js
@@ -0,0 +1,9 @@
+const express= require('express')
+
+const router = express.Router();
+
+router.get('/home', (req,res)=>{
+    res.send('Hi')
+})
+
+module.exports= router;
